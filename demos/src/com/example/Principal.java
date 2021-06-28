c0fc650e4e1d0076a5afb12b5a84d79e4f12cc4a
Javier <jmartin@grupoloyal.com>
2021-06-28 23:40:05 +0200

    Documentación

---
+++ demos/src/com/example/Principal.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/**
+ * Clase lanzadora de los ejemplos del curso de Java
+ * @author Javier
+ *
+ */
+public class Principal {
+
+	/**
+	 * Metodo inicial de la aplicacion
+	 * @param args Linea de comandos
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola miranda"); 
+	}
+
+}
