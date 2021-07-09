d706e360e6ece328ed3cc5c99c461e2e41ae31b9
Javier <jmartin@grupoloyal.com>
2021-07-09 13:39:14 +0200

    Consulta 1

--- datos/src/com/example/Principal.java
+++ datos/src/com/example/Principal.java
@@ -1,10 +1,40 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		consulta1();
 
+	}
+
+	private static void consulta1() {
+		try(Connection connection = JDBCBridged.getConnection()) {
+			Statement cmd = connection.createStatement();
+			ResultSet rs = cmd.executeQuery("SELECT film_id, title FROM film");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString("title"));
+			}
+			rs.close();
+			cmd.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+//		try {
+//			Connection connection = JDBCBridged.getConnection();
+//			try {
+//				// ...
+//			} finally {
+//				connection.close();
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
