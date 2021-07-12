1c082a35f375866af9072a4ffd329e28e60c27f2
Javier <jmartin@grupoloyal.com>
2021-07-12 12:37:13 +0200

    Hola mundo

--- mvc/src/main/java/com/example/MvcApplication.java
+++ mvc/src/main/java/com/example/MvcApplication.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvcApplication {
+public class MvcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ya estamos");		
 	}
 
 }
