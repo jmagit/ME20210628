46e2b443767126d506bda08804971e1ac716d014
Javier <jmartin@grupoloyal.com>
2021-07-15 13:53:55 +0200

    Respuesta

---
+++ demo-jee/src/main/java/com/example/presentation/servlets/Saluda.java
@@ -0,0 +1,64 @@
+package com.example.presentation.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Saluda
+ */
+public class Saluda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String nombrePorDefecto = "";
+	/**
+	 * Default constructor.
+	 */
+	public Saluda() {
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		nombrePorDefecto = config.getInitParameter("nombre");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setHeader("X-La-Mia", "una cabecera");
+		PrintWriter out = response.getWriter();
+
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='UTF-8'>");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+		out.println("<title>JEE</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hola " + nombrePorDefecto + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
